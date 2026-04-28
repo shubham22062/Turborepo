2f211b2e5450b27a339283b92eac0c031c90fbc5
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-04-28 14:49:23 +0530

    changes

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import {Admin} from "@repo/ui/admin"
+
+
+export default function admin(){
+    return(
+       <div>
+        <h1>"Hello this is ypur shubham"</h1>
+        <Admin/>
+       </div>
+    )
+}
---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,7 @@
+"use-client"
+
+export const Admin = ()=>{
+    return(
+        <h1>hi from the admin</h1>
+    )
+}
